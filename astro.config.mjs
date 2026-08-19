16d04191130bc04a1780f62a6eb4fec3f936958f
aistari <301959373+odysstari@users.noreply.github.com>
2026-08-18 20:47:21 -0400

    Add tailwindcss and daisyui dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://scefi-umss.github.io',
   base: '/scefi',
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
